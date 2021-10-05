32f842877586ebafd984b6625828c5752b244eec
Taseer <taseer.ahmadd@gmail.com>
2021-10-06 03:07:01 +0500

    init commit

---
+++ script.js
@@ -0,0 +1,205 @@
+const selectedNotes = [];
+
+window.onload = function () {
+    if (!isStorageEmpty()) {
+        loadSavedNotes();
+        clearNoteSession();
+    }
+}
+
+document.getElementById('note-form').oninput = () => {
+    window.sessionStorage.setItem('creating', 'true')
+}
+
+const submitBtn = document.getElementById('submitBtn');
+
+submitBtn.onclick = () => {
+    const noteId = getNoteId();
+    if (noteId === null) {
+        alert('Browser does not supports localStorage. Unable to create a new note.')
+        return
+    }
+
+    createNoteSession(noteId);
+    //noteData['id'] = noteId;
+
+    if (isNoteEmpty()) {
+        alert('Can not save an empty note!');
+        return
+    }
+
+    saveNoteToStorage();
+    displayNote();
+
+    clearNoteSession();
+    clearForm();
+    showNoteSaved();
+}
+
+function displayNote() {
+    const id = window.sessionStorage.getItem('id');
+
+    let noteView = createNoteView(id);
+    attachDataToView(noteView);
+    attachNoteClickListener(noteView);
+    document.getElementById('notes-list').appendChild(noteView);
+}
+
+function clearNoteSession() {
+    window.sessionStorage.clear();
+}
+
+function saveNoteToStorage() {
+    const id = window.sessionStorage.getItem('id');
+    const text = window.sessionStorage.getItem('text');
+    const title = window.sessionStorage.getItem('title');
+
+    console.log(title + ':' + text);
+
+    window.localStorage.setItem(id, title + ':' + text);
+}
+
+function loadSavedNotes() {
+    Object.keys(window.localStorage).forEach(function (key) {
+        if (key !== 'row-key') {
+            const note = window.localStorage.getItem(key);
+            const noteFields = note.split(':');
+    
+            saveNoteToSession(key, noteFields[0], noteFields[1]);
+            displayNote();
+            clearNoteSession();
+        }
+    });
+}
+
+function isStorageEmpty() {
+    if (!browserHasStorage()) {
+        alert('Browser does not have storage. Notes can not be created!');
+        return
+    }
+
+    return window.localStorage.length === 0;
+}
+
+function attachNoteClickListener(noteItem) {
+    noteItem.onclick = () => {
+        const item = document.getElementById(noteItem.id);
+
+        const isSelected = item.getAttribute('rel');
+        if (isSelected === '1') {
+            removeSelectedItem(item.id);
+
+            item.style.border = 'none';
+            item.setAttribute('rel', '0');
+            return
+        }
+
+        addSelectedItem(item.id)
+        item.setAttribute('rel', '1');
+
+        item.style.border = '4px solid black';
+    }
+}
+
+function removeSelectedItem(id) {
+    const index = selectedNotes.indexOf(id);
+    if (index !== -1) {
+        selectedNotes.splice(index, 1);
+    }
+}
+
+function addSelectedItem(id) {
+    selectedNotes.push(id);
+}
+
+function showNoteSaved() {
+    const noteState = document.getElementById('note-state');
+    noteState.innerHTML = 'Note saved!';
+    setTimeout(() => {
+        noteState.innerHTML = 'New note';
+    }, 1500);
+}
+
+function clearForm() {
+    window.sessionStorage.setItem('creating', 'false');
+
+    document.getElementById('note-form').reset();
+}
+
+function isNoteEmpty() {
+    let text = window.sessionStorage.getItem('text');
+    let title = window.sessionStorage.getItem('title');
+
+    return text.length === 0 && title.length === 0;
+}
+
+function browserHasStorage() {
+    return !(typeof (Storage) === 'undefined');
+}
+
+function getNoteId() {
+    if (!browserHasStorage()) {
+        return null;
+    }
+
+    let key = window.localStorage.getItem('row-key');
+
+    // console.log('ROW-KEY' + ' ' + key);
+
+    if (!key) {
+        window.localStorage.setItem('row-key', '0')
+    }
+
+    key = window.localStorage.getItem('row-key');
+    window.localStorage.setItem('row-key', ++key);
+
+    //console.log('ROW-KEY' + ' ' + window.localStorage.getItem('row-key'));
+
+    return key++;
+}
+
+function createNoteSession(noteId) {
+    let formTitle = document.querySelector('#note-title').value.trim();
+    let formText = document.querySelector('#note-text').value.trim();
+
+    saveNoteToSession(noteId, formTitle, formText);
+}
+
+function saveNoteToSession(id, title, text) {
+    window.sessionStorage.setItem('id', id);
+    window.sessionStorage.setItem('text', text);
+    window.sessionStorage.setItem('title', title);
+}
+
+function createNoteView(id) {
+    const textView = document.createElement('p');
+    const titleView = document.createElement('p');
+    const noteContainer = document.createElement('div');
+
+    textView.classList.add('text');
+    titleView.classList.add('title');
+    noteContainer.classList.add('note');
+
+    noteContainer.id = id;
+    noteContainer.setAttribute('rel', '0');
+
+    noteContainer.appendChild(titleView);
+    noteContainer.appendChild(textView);
+
+    noteContainer.style.backgroundColor = 'grey'
+    noteContainer.style.width = '100%'
+    noteContainer.style.height = 'auto'
+
+    return noteContainer;
+}
+
+function attachDataToView(noteView) {
+    const text = window.sessionStorage.getItem('text');
+    const title = window.sessionStorage.getItem('title');
+
+    const items = noteView.childNodes;
+    items[0].innerHTML = title;
+    items[1].innerHTML = text;
+
+    return noteView;
+}
